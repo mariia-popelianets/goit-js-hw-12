98c1a9fabe3c99b5dcd75098e9e794471758ccea
Mariia Popelianets <mariakh0128@gmail.com>
2026-05-27 23:46:52 +0300

    fix: align structure with goit template and fix build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  base: '/goit-js-hw-12/',
-  define: {
-    // Вказуємо Vite, що global — це насправді window в браузері
-    global: 'window',
-  },
-  build: {
-    rollupOptions: {
-      input: './index.html',
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+      global: 'window', // Вирішує помилку з SimpleLightbox
     },
-  },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          assetFileNames: 'assets/[name]-[hash][extname]',
+          chunkFileNames: 'assets/[name]-[hash].js',
+          entryFileNames: 'assets/[name]-[hash].js',
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
