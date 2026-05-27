e65500be4a3025573034d1367d2fa92eb9ad8f64
Mariia Popelianets <mariakh0128@gmail.com>
2026-05-27 23:59:01 +0300

    fix: correct css path in index.html for production build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-js-hw-12/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
       global: 'window', // Вирішує помилку з SimpleLightbox
